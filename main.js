2696abce47cb3f64dbbd5214a45afed63f1b3ded
Eunice Ogboye <ogboyeeunice@gmail.com>
2023-02-15 13:55:47 -0800

    upate

---
+++ main.js
@@ -0,0 +1,24 @@
+const carousel = document.querySelector(".carousel"),
+arrowIcons = document.querySelector(".wrapper i");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.scrollLeft = e.pageX;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
+
