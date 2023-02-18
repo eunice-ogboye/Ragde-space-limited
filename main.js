50181eec5c24cbd5d0b832c4189bf941ebe1fc3f
Eunice Ogboye <ogboyeeunice@gmail.com>
2023-02-18 23:48:32 +0100

    model

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const carousel = document.querySelector(".carousel"),
-arrowIcons = document.querySelector(".wrapper i");
+firstImg = carousel.querySelectorAll("img")[0];
+arrowIcons = document.querySelectorAll(".wrapper i");
 
 let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 14;
 
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth
+    })
+})
 const dragStart = (e) => {
     isDragStart = true;
     prevPageX = e.pageX;
@@ -12,11 +19,14 @@ const dragStart = (e) => {
 const dragging = (e) => {
     if(!isDragStart) return;
     e.preventDefault();
-    carousel.scrollLeft = e.pageX;
+    carousel.classList.add("dragging");
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
 const dragStop = () => {
     isDragStart = false;
+    carousel.classList.remove("dragging");
 }
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
